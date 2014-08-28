663683610a8547819db6ed6a30a32a20f518963d
Michael Hickman <mhickman@unm.edu>
2014-08-27 18:31:16 -0600

    homepage

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 	// Resize welcome element on load
 	var welcome = $('#welcome');
-	height = $(window).height() - 100;
+	height = $(window).height(); //-100;
 	welcome.css('height', height)
+
+	// Center Welcome content
+	var content = $('#welcome-content');
+	content_height = content.height();
+	diff = height - content_height;
+	content.css("padding-top", diff/2 - content_height/2);
 })
