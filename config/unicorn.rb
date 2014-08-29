12176faa311436bb2bdbd262901e75b293e3adfd
Michael Hickman <mhickman@unm.edu>
2014-08-28 20:40:09 -0600

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,14 @@
-#root = "/home/apps/DelNorte/current"
-#working_directory root
+@dir = "/home/apps/DelNorte/current"
+working_directory @dir
 
 worker_processes 3
 timeout 30
 preload_app true
+
+
+# Set process id path
+pid "#{@dir}tmp/pids/unicorn.pid"
+
+# Set log file paths
+stderr_path "#{@dir}log/unicorn.stderr.log"
+stdout_path "#{@dir}log/unicorn.stdout.log"
