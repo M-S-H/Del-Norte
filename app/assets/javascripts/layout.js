a4ff7eb55a09a8feec03743e34b5f67675ad3df2
Michael Hickman <mhickman@unm.edu>
2014-10-13 21:30:01 -0600

    mobile nav

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
+	welcome();
+
+	jQuery(window).on( "orientationchange", function() {
+		welcome();
+	});
+});
+
+
+function welcome() {
 	// Resize welcome element on load
 	var welcome = $('#welcome');
-	height = $(window).height(); //-100;
+	height = $(window).height();
 	welcome.css('height', height)
+
 
 	// Center Welcome content
 	var content = $('#welcome-content');
 	content_height = content.height();
 	diff = height - content_height;
 	content.css("padding-top", diff/2 - content_height/2);
-})
+}
