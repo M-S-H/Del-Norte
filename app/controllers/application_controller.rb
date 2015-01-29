70076e7a392321f1365a034f59337d6c041c9ea7
Michael Hickman <mhickman@unm.edu>
2015-01-28 19:26:23 -0700

    link fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
 	def download_feb
 		send_file(
 			"public/Feburary2015.pdf",
-			filename: "Feburary2015.pdf",
+			filename: "February2015.pdf",
 			type: "application/pdf"
 		)
 	end
