45f1648771318315878b1dfd032ddf8f2fc2a570
Michael Hickman <mhickman@unm.edu>
2015-06-06 14:34:11 -0600

    bug fix

--- config/initializers/youtube.rb
+++ config/initializers/youtube.rb
@@ -1,7 +1,8 @@
 require 'yt'
 
 Yt.configure do |config|
-	config.api_key = ENV['YOUTUBE_PUBLIC']
+	#config.api_key = ENV['YOUTUBE_PUBLIC']
+	config.api_key = 'AIzaSyAbSP0sadpb6TXOa7gw59U-Vogi8Qh3ePw'
 end
 
 SERMONS = Yt::Playlist.new id: 'PLZLN8ggsIxePmqamBkTYJEYHY-3sgenkQ'
