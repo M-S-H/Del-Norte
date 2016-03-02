446f37d30cbedbb66c4cdd333893fe58fcc34a96
Michael Hickman <mhickman@unm.edu>
2016-03-02 11:17:12 -0700

    player

--- app/assets/javascripts/sermons.js
+++ app/assets/javascripts/sermons.js
@@ -18,39 +18,57 @@ function initblur() {
 }
 
 
-function blurimage() {
-	canvas = document.getElementById('sermon-canvas');
-	context = canvas.getContext('2d');
-	image = document.getElementById('s-bg-image');
-	context.drawImage(image,0,0, 800, 800);
-	stackBlurImage("s-bg-image", "sermon-canvas", 100, 1);
-	$('#sermon-bg').css("backgroundImage", "url(" + canvas.toDataURL() + ")");
-}
+// function blurimage() {
+// 	canvas = document.getElementById('sermon-canvas');
+// 	context = canvas.getContext('2d');
+// 	image = document.getElementById('s-bg-image');
+// 	context.drawImage(image,0,0, 800, 800);
+// 	stackBlurImage("s-bg-image", "sermon-canvas", 100, 1);
+// 	$('#sermon-bg').css("backgroundImage", "url(" + canvas.toDataURL() + ")");
+// }
 
 
-function draw() {
-	canvas = document.getElementById('sermon-canvas');
-	context = canvas.getContext('2d');
-	image = document.getElementById('0');
-	context.drawImage(image,0,0, 2000, 300);
-}
+// function draw() {
+// 	canvas = document.getElementById('sermon-canvas');
+// 	context = canvas.getContext('2d');
+// 	image = document.getElementById('0');
+// 	context.drawImage(image,0,0, 2000, 300);
+// }
 
 
 $(document).ready(function() {
-	// Change Sermons
+		// Change Sermons
 	$('.sermon-listing').click(function() {
 		$("html, body").animate({ scrollTop: 0 }, "slow");
-		$.ajax({
-			url: '/change_sermon',
-			data: { id: $(this).attr("id") },
-			success: function (response) {
-				//alert("success");
-			},
-			error: function () {
-				//alert("error");
-			}
-		}); 
+		
+		$("#sermon-player h3").html($(this).attr('name'));
+		$("#audiosource source").attr("src", $(this).attr("audio"));
+		$("#s-bg-image").attr("src", $(this).attr("image"));
+
+		if (!player.paused) {
+			toggleplayer();
+		}
+
+		initblur();
+		player.load();
+		reset();
 	});
+
+
+	// // Change Sermons
+	// $('.sermon-listing').click(function() {
+	// 	$("html, body").animate({ scrollTop: 0 }, "slow");
+	// 	$.ajax({
+	// 		url: '/change_sermon',
+	// 		data: { id: $(this).attr("id") },
+	// 		success: function (response) {
+	// 			//alert("success");
+	// 		},
+	// 		error: function () {
+	// 			//alert("error");
+	// 		}
+	// 	}); 
+	// });
 });
 
 
